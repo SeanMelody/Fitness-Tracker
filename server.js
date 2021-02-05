c875f602ad3cfde5a55554b843695ecaf75e283f
Sean Melody <seandmelody@gmail.com>
2021-02-05 10:58:19 -0800

    mongoose connection adjusted

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+// Required Consts!
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3003;
 
-// const User = require("./userModel.js");
+// const db = require("./seeders/seed.js");
 
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Require static files so CSS and JS work!
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+// Connect to mongoose and optimize for Heroku Deployment
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 // Start the server
 app.listen(PORT, () => {
